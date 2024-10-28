e648ad8ce35981df03ad7c8709e625b5e39d3f32
Giuseppe Cangemi <giuseppecangemi94@gmail.com>
2024-10-28 23:27:48 +0100

    uploads.html introduction

---
+++ DRA.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Cambia directory al percorso dello script
+cd "/Users/giuseppecangemi/Desktop/Programming/Python/DerryRockAutomation"
+# Esegui lo script Python
+python3 automazione.py
