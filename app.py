ec8c34074740d988df65c527bef1d96deb3417a2
Giuseppe Cangemi <giuseppecangemi94@gmail.com>
2024-11-07 22:52:04 +0100

    modifica porta

--- app.py
+++ app.py
@@ -181,5 +181,6 @@ def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000)) 
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+
