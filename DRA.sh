8defad3c9de2613bc950c06eabf99f63d54648d4
Giuseppe Cangemi <giuseppecangemi94@gmail.com>
2024-11-08 01:11:20 +0100

    cleaning files

--- DRA.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Cambia directory al percorso dello script
-cd "/Users/giuseppecangemi/Desktop/Programming/Python/DerryRockAutomation"
-# Esegui lo script Python
-python3 automazione.py
